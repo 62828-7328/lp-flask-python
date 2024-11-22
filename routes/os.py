ba8d49c99832fe998164c75b0541241966104285
Máximo Bern. <71853332+BurnMaximo@users.noreply.github.com>
2024-11-21 20:03:11 -0400

    Update os.py

--- routes/os.py
+++ routes/os.py
@@ -17,3 +17,32 @@ def register_os():
     db.session.commit()
 
     return jsonify({'id': os.id, 'proprietary_name': os.proprietary_name, 'number_fone': os.number_fone, 'device': os.device, 'problem': os.problem}), 201
+
+@app.route("/os", methods=["GET"])
+def list_os():
+    query_params = request.args
+
+    page = query_params.get('page', default=0, type=int)
+    limit = query_params.get('limit', default=10, type=int)
+    offset = page * limit
+
+    filter = {}
+    ignored_fields = ['page', 'limit', 'sort_by', 'sort_direction']
+    for field, value in query_params.items():
+        if field not in ignored_fields:
+            filter[field] = value
+
+    sort_by = query_params.get('sort_by', default='id', type=str)
+    sort_direction = query_params.get('sort_direction', default='asc', type=str)
+
+    order_by = asc(sort_by) if sort_direction == 'asc' else desc(sort_by)
+
+    os = OS.query.filter_by(**filter).order_by(order_by).offset(offset).limit(limit).all()
+    if not os:
+        return jsonify([]), 200
+
+    status_code = 206 if len(os) == limit else 200
+
+    result = [{'id': user.id, 'username': user.username, 'email': user.email} for user in os]
+
+    return jsonify({'id': os.id, 'proprietary_name': os.proprietary_name, 'number_fone': os.number_fone, 'device': os.device, 'problem': os.problem}), status_code
