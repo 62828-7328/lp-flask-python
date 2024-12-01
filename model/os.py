8cfe4f6726850e49ceb1cb8072bf306728100cf3
Máximo Bern. <gustavomaximo072@gmail.com>
2024-12-01 15:21:53 -0400

    Reorganização

--- model/os.py
+++ model/os.py
@@ -2,10 +2,10 @@ from app import db
 
 class OS(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    proprietary_name = db.Column(db.String(80), unique=True, nullable=False)
+    proprietary_name = db.Column(db.String(80), unique=False, nullable=False)
     number_fone = db.Column(db.Integer(15), nullable=False)
     device = db.Column(db.String(80), nullable=False)
-    problem = db.Column(db.String(200), unique=True, nullable=False)
+    problem = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return '<OS %r>' % self.proprietary_name
--- routes/user.py
+++ routes/user.py
@@ -2,8 +2,20 @@ from flask import request, jsonify
 from sqlalchemy import asc, desc
 
 from app import app
+from app import db
+from model import user
 from model.user import User
 
+@app.route("/users", methods=["POST"])
+def register_user():
+    data = request.get_json()
+    new_user = User(username=data['username'],
+                    email=data['email'],
+                    password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'id': user.id, 'username': user.username, 'email': user.email}), 201
 
 @app.route("/users", methods=["GET"])
 def list_users():
